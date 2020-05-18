bff07fd37336b2172cf653eb1c43e940b7709d1a
Roman Grubic <grubic.roman@gmail.com>
2020-05-18 13:01:42 +0000

    Setting media folder and adding data.

--- realtors/models.py
+++ realtors/models.py
@@ -5,12 +5,12 @@ from datetime import datetime
 # Create your models here.
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/realtors')
+    photo = models.ImageField(upload_to='photos/realtors/%Y/%m/%d/')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
-
+    
     def __str__(self):
         return self.name
