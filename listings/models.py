722381b8f0353c2f7723bf73cc243f01366afbb2
Roman Grubic <grubic.roman@gmail.com>
2020-05-18 14:38:41 +0000

    Pulling Listings data from DB.

--- listings/models.py
+++ listings/models.py
@@ -33,6 +33,6 @@ class Listing(models.Model):
         upload_to='photos/listing/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
-
+    
     def __str__(self):
         return self.title
