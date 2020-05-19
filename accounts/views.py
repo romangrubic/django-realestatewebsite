6dbb239425fb9faf82fd4577da9d50eeee724bcd
Roman Grubic <grubic.roman@gmail.com>
2020-05-19 14:37:52 +0000

    Added register and login templates.

--- accounts/views.py
+++ accounts/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        #Register user
+        print('SUBMIT')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        #Login user
+        print('LOGIN')
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
