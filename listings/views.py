95448f340339556c952b3391e3aa461cd4ecfee3
Roman Grubic <grubic.roman@gmail.com>
2020-05-19 13:44:51 +0000

    Search form filters.

--- listings/views.py
+++ listings/views.py
@@ -28,9 +28,42 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords icontains = word is contained in selected property
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City iexact = exact match
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # County
+    if 'county' in request.GET:
+        county = request.GET['county']
+        if county:
+            queryset_list = queryset_list.filter(county__iexact=county)
+
+    # Bedrooms lte = less than or equal to
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
     context = {
         'county_choices': county_choices,
         'price_choices': price_choices,
-        'bedroom_choices': bedroom_choices
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list
     }
     return render(request, 'listings/search.html', context)
