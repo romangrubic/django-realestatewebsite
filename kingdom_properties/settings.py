3f864516f9bad9a2935a86ce7f3837da61f56de5
Roman Grubic <grubic.roman@gmail.com>
2020-05-20 19:10:42 +0000

    Added procfile and requirements for heroku.

--- kingdom_properties/settings.py
+++ kingdom_properties/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import env
+import dj_database_url
 from django.contrib.messages import constants as messages
 
 
@@ -83,13 +84,15 @@ WSGI_APPLICATION = 'kingdom_properties.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
